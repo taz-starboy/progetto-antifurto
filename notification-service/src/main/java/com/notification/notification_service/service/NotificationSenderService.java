a0e9546dc288e05804ab9802a1b740c3c90feff2
marcos <taz23@hotmail.it>
2025-08-02 10:17:42 +0200

    add twilio dependency and implement whatsapp sending method

--- notification-service/src/main/java/com/notification/notification_service/service/NotificationSenderService.java
+++ notification-service/src/main/java/com/notification/notification_service/service/NotificationSenderService.java
@@ -1,6 +1,9 @@
 package com.notification.notification_service.service;
 
 import com.notification.notification_service.dto.NotificationRequest;
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,18 +21,39 @@ public class NotificationSenderService {
     @Value("${spring.mail.from}")
     private String mailFrom;
 
+    @Value("${twilio.service.account.sid}")
+    private String twilioSID;
+
+    @Value("${twilio.service.auth.token}")
+    private String twilioAuthToken;
+
+    @Value("${twilio.service.from.whatsapp}")
+    private String whatsappFrom;
+
+    @Value("${notification.service.tel.test}")
+    private String testTel;
+
     private final JavaMailSender mailSender;
+
 
     public String sendNotification(NotificationRequest request, String notification) {
 
         if (request.getDestinationType().equals("email")) {
             return sendEmail(request.getDestination(), notification);
         }
-        return sendWhatsApp(request.getDestination());
+        return sendWhatsApp(request.getDestination(), notification);
     }
 
-    private String sendWhatsApp(@NotBlank String destination) {
-        return "\nWhatsapp sender not yet available.";
+    private String sendWhatsApp(@NotBlank String destination, String notification) {
+        Twilio.init(this.twilioSID, this.twilioAuthToken);
+
+        Message message = Message.creator(
+                new PhoneNumber("whatsapp:" + this.testTel),
+                new PhoneNumber(this.whatsappFrom),
+                notification
+        ).create();
+
+        return "\nWhatsApp message sent: " + message.getSid();
     }
 
     private String sendEmail(@NotBlank String destination, String notification) {
